993896ac5af4330ec352851c3a3ac99021705ac4
Saksham Paliwal <83136084+sakshampaliwal@users.noreply.github.com>
2023-04-18 22:55:45 +0530

    Script to print Output on screen

---
+++ Sc1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#The above line is a shebang line. It specifies the interpreter to be used to execute the script, in this case, the Bash shell.
+#It is best practice to use this line as it tells which shell to use, it is not strictly necessary
+
+echo Saksham Paliwal is learning bash scripting
+echo DevOps and Cloud Computing
