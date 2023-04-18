2b5aef95457363732414459ca8e84bd959cae4f0
Saksham Paliwal <83136084+sakshampaliwal@users.noreply.github.com>
2023-04-19 00:19:50 +0530

    loops.sh

--- loops.sh
+++ loops.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#For Loop
+
 echo First Loop...
 for num in 1 2 3 4
 do
@@ -16,4 +18,16 @@ echo Third Loop...
 for t in Eat Sleep Repeat
 do
         echo Task is $t
+done
+
+#While loops:
+
+echo While Loops........
+num=10
+count=10
+
+while [ $count -le $num ]
+do
+        echo $count
+        let count++
 done
