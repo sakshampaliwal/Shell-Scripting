b85c1589e9363c559b59c324b1b210f4d41cebfb
Saksham Paliwal <83136084+sakshampaliwal@users.noreply.github.com>
2023-04-18 23:10:39 +0530

    Variable.sh

---
+++ Variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name=saksham
+age=19
+
+echo My name is $name and my age is $age
+
+host=$(hostname) #we can also store command on variables  by adding $ sign and with brackets
+echo my hostname is $host
